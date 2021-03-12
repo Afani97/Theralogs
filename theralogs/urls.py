b1a02a340bddcdf31a1b96a8d3866b7e6ee0488b
Aristotel Fani <afani1997@gmail.com>
2021-03-12 16:32:44 -0500

    add forgot password flow

--- theralogs/urls.py
+++ theralogs/urls.py
@@ -1,7 +1,8 @@
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
+
+from django.contrib.auth import views as auth_views
 
 from django.urls import path
 from .views import main, auth, profile, patient, rn
@@ -48,6 +49,34 @@ urlpatterns = [
         name="rn_resend_email",
     ),
     path("contact-us/", main.contact_us, name="contact_us"),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="theralogs/registration/password_reset_form.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="theralogs/registration/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="theralogs/registration/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="theralogs/registration/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
 
 # JWT
