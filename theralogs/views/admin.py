3141ba67a88b0e5aaf232db390fa36c8d01dfe47
Aristotel Fani <afani1997@gmail.com>
2021-03-19 11:00:32 -0400

    add admin page to view analytics

---
+++ theralogs/views/admin.py
@@ -0,0 +1,56 @@
+from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
+from django.db.models import Count, Sum
+from django.shortcuts import render
+from ..models import TLSession, Therapist, Patient
+from datetime import datetime
+
+
+def refund_charge(modeladmin, request, queryset):
+    for session in queryset:
+        refunded = session.refund_charge()
+        print(refunded)
+        if refunded is True:
+            messages.add_message(request, messages.WARNING, "Session already refunded")
+        elif refunded is False:
+            messages.add_message(request, messages.SUCCESS, "Session has been refunded")
+            session.refunded = True
+            session.save()
+        else:
+            messages.add_message(
+                request, messages.ERROR, "Session unable to be refunded"
+            )
+
+
+refund_charge.short_description = "Refund session"
+
+
+@staff_member_required
+def view_analytics(request):
+    therapist_count = Therapist.objects.count()
+    patient_count = Patient.objects.count()
+    current_date = datetime.now()
+    sessions_today_count = (
+        TLSession.objects.filter(created_at__year=current_date.year)
+        .filter(created_at__month=current_date.month)
+        .filter(created_at__day=current_date.day)
+        .count()
+    )
+    total_hourly_session_for_month_annotate = (
+        TLSession.objects.filter(created_at__year=current_date.year)
+        .filter(created_at__month=current_date.month)
+        .aggregate(session_seconds=Sum("recording_length"))
+    )
+    total_hourly_session_for_month = (
+        total_hourly_session_for_month_annotate["session_seconds"] / 3600
+    )
+    goal_hours_per_month = 2000
+    percentage_to_goal = (total_hourly_session_for_month / goal_hours_per_month) * 100
+    context = {
+        "therapist_count": therapist_count,
+        "patient_count": patient_count,
+        "sessions_today_count": sessions_today_count,
+        "total_session_hours": "{:.2f}".format(total_hourly_session_for_month),
+        "percentage_to_goal": "{:.2f}".format(percentage_to_goal),
+    }
+    return render(request, "admin/analytics.html", context)
--- theralogsproject/urls.py
+++ theralogsproject/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django_otp.admin import OTPAdminSite
-from django.contrib import admin, messages
+from django.contrib import admin
 from django.contrib.auth.models import User
-from django_otp.plugins.otp_totp.models import TOTPDevice
+from django_otp.admin import OTPAdminSite
 from django_otp.plugins.otp_totp.admin import TOTPDeviceAdmin
+from django_otp.plugins.otp_totp.models import TOTPDevice
+
 from theralogs.models import TLSession, Therapist, Patient
+from theralogs.views.admin import refund_charge, view_analytics
 
 
 class OTPAdmin(OTPAdminSite):
@@ -35,25 +37,6 @@ admin_site.register(Therapist)
 admin_site.register(Patient)
 
 
-def refund_charge(modeladmin, request, queryset):
-    for session in queryset:
-        refunded = session.refund_charge()
-        print(refunded)
-        if refunded is True:
-            messages.add_message(request, messages.WARNING, "Session already refunded")
-        elif refunded is False:
-            messages.add_message(request, messages.SUCCESS, "Session has been refunded")
-            session.refunded = True
-            session.save()
-        else:
-            messages.add_message(
-                request, messages.ERROR, "Session unable to be refunded"
-            )
-
-
-refund_charge.short_description = "Refund session"
-
-
 class TLSessionAdmin(admin.ModelAdmin):
     list_display = ["id", "created_at"]
     actions = [
@@ -68,6 +51,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path("admin/", admin_site.urls), path("", include("theralogs.urls"))]
+
+urlpatterns = [
+    path("admin/analytics/", view_analytics, name="theralogs_analytics"),
+    path("admin/", admin_site.urls),
+    path("", include("theralogs.urls")),
+]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
